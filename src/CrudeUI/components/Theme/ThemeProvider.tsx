22d1ec0d3fb144d9bb0d52650b55ed8a83a40adf
Jeanette Yue <j-yue>
2023-08-08 17:56:58 -0700

    fix: theme props should be optional since default param provided

--- src/CrudeUI/components/Theme/ThemeProvider.tsx
+++ src/CrudeUI/components/Theme/ThemeProvider.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from "./GlobalStyle";
 import { theme as defaultTheme, Theme } from "../../theme/theme";
 
 interface ThemeProviderProps {
-  theme: Theme;
+  theme?: Theme;
   children: React.ReactNode;
   /** general css styles to add */
   styles?: string;
