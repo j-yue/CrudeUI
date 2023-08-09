7a3b53da82f8ccf5dcf95266c8227eb72d04a80b
Jeanette Yue <j-yue>
2023-08-08 17:39:10 -0700

    Refactor type FontStyle from array to object with keys weight, size, and height

--- src/CrudeUI/theme/design-tokens/typography.ts
+++ src/CrudeUI/theme/design-tokens/typography.ts
@@ -1,9 +1,14 @@
 //font weight, font size, line height
-export type FontStyle = [weight: number, size: string, height: string];
+// export type FontStyle = [weight: number, size: string, height: string];
+export type FontStyle = {
+  weight: number;
+  size: string;
+  height: string;
+};
 
 // headings have levels 1-6, most other types have 2 emphasis levels
-type HeadingScale = "1" | "2" | "3" | "4" | "5" | "6";
-type ContentScale = "high" | "regular";
+export type HeadingScale = "1" | "2" | "3" | "4" | "5" | "6";
+export type ContentScale = "high" | "regular";
 
 export type Heading = {
   [Scale in HeadingScale]: FontStyle;
@@ -14,54 +19,110 @@ export type Content = {
 };
 
 export const headingSm: Heading = {
-  1: [700, "3rem", "3.75rem"],
-  2: [700, "2.5rem", "3rem"],
-  3: [600, "2.25rem", "2.75rem"],
-  4: [600, "2rem", "2.5rem"],
-  5: [600, "1.75rem", "2.25rem"],
-  6: [600, "1.5rem", "2rem"],
+  1: {
+    weight: 700,
+    size: "3rem",
+    height: "3.75rem",
+  },
+  2: {
+    weight: 700,
+    size: "2.5rem",
+    height: "3rem",
+  },
+  3: {
+    weight: 600,
+    size: "2.25rem",
+    height: "2.75rem",
+  },
+  4: {
+    weight: 600,
+    size: "2rem",
+    height: "2.5rem",
+  },
+  5: {
+    weight: 600,
+    size: "1.75rem",
+    height: "2.25rem",
+  },
+  6: {
+    weight: 600,
+    size: "1.5rem",
+    height: "2rem",
+  },
 };
 
 export const headingLg: Heading = {
-  1: [700, "5rem", "6rem"],
-  2: [700, "4.25rem", "5.25rem"],
-  3: [600, "3.75rem", "4.5rem"],
-  4: [600, "3.25rem", "4rem"],
-  5: [600, "2.75rem", "3.25rem"],
-  6: [600, "2.25rem", "2.75rem"],
+  1: {
+    weight: 700,
+    size: "5rem",
+    height: "6rem",
+  },
+  2: {
+    weight: 700,
+    size: "4.25rem",
+    height: "5.25rem",
+  },
+  3: {
+    weight: 600,
+    size: "3.75rem",
+    height: "4.5rem",
+  },
+  4: {
+    weight: 600,
+    size: "3.25rem",
+    height: "4rem",
+  },
+  5: {
+    weight: 600,
+    size: "2.75rem",
+    height: "3.25rem",
+  },
+  6: {
+    weight: 600,
+    size: "2.25rem",
+    height: "2.75rem",
+  },
 };
 
 export const titleSm: Content = {
-  high: [600, "1.25rem", "1.75rem"],
-  regular: [400, "1.25rem", "1.75rem"],
+  high: { weight: 600, size: "1.25rem", height: "1.75rem" },
+  regular: { weight: 400, size: "1.25rem", height: "1.75rem" },
 };
 
 export const titleLg: Content = {
-  high: [600, "1.75rem", "2.25rem"],
-  regular: [400, "1.75rem", "2.25rem"],
+  high: { weight: 600, size: "1.75rem", height: "2.25rem" },
+  regular: { weight: 400, size: "1.75rem", height: "2.25rem" },
 };
 
 export const substitleSm: Content = {
-  high: [600, "1.125rem", "1.5rem"],
-  regular: [400, "1.125rem", "1.5rem"],
+  high: { weight: 600, size: "1.125rem", height: "1.5rem" },
+  regular: { weight: 400, size: "1.125rem", height: "1.5rem" },
 };
 
 export const subtitleLg: Content = {
-  high: [600, "1.5rem", "2rem"],
-  regular: [400, "1.5rem", "2rem"],
+  high: { weight: 600, size: "1.5rem", height: "2rem" },
+  regular: { weight: 400, size: "1.5rem", height: "2rem" },
 };
 
-export const buttonSm: FontStyle = [700, "1.125rem", "1.5rem"];
+export const buttonSm: FontStyle = {
+  weight: 700,
+  size: "1.125rem",
+  height: "1.5rem",
+};
 
-export const buttonLg: FontStyle = [700, "1.5rem", "2rem"];
+export const buttonLg: FontStyle = {
+  weight: 700,
+  size: "1.5rem",
+  height: "2rem",
+};
 
 export const captionSm: Content = {
-  high: [600, "1rem", "1.25rem"],
-  regular: [400, "1rem", "1.25rem"],
+  high: { weight: 600, size: "1rem", height: "1.25rem" },
+  regular: { weight: 400, size: "1rem", height: "1.25rem" },
 };
 export const captionLg: Content = {
-  high: [600, "1.25rem", "1.5rem"],
-  regular: [400, "1.25rem", "1.5rem"],
+  high: { weight: 600, size: "1.25rem", height: "1.5rem" },
+  regular: { weight: 400, size: "1.25rem", height: "1.5rem" },
 };
 
 export type Font = {
