e457a451dfa95d17d35532840771736163cccfcb
Jeanette Yue <j-yue>
2023-08-31 14:34:10 -0700

    Replace hard coded sizes with responsive styles

--- src/CrudeUI/components/Icon/Icon.tsx
+++ src/CrudeUI/components/Icon/Icon.tsx
@@ -1,32 +1,27 @@
 //icon is aspect ratio image with 1:1 dimensions
 //default is that it is a decorative icon, so empty alt
 import { styled } from "styled-components";
-
-type IconSizes = "sm" | "md";
+import { responsiveWrapper } from "../../utilities/styleHelpers";
 
 interface IconProps {
-  /** size of icon */
-  size: IconSizes;
   /** image or svg to be used as icon */
   children: React.ReactNode;
 }
 
-type ImgContainerProps = {
-  size: IconSizes;
-};
+const setIconDimensions = (size: number) =>
+  `width: ${size}rem;\nheight: ${size}rem;`;
 
-const IconContainer = styled.span<ImgContainerProps>`
-  ${({ size }) => {
-    const width = size === "sm" ? "1.5rem" : "2rem";
+const IconContainer = styled.span`
+  ${({ theme }) => {
     return `
       color: inherit;
       display: inline-block;
-      width: ${width};
-      height: ${width};
+      ${setIconDimensions(1.5)}
+      ${responsiveWrapper(theme.screens.large, setIconDimensions(2))}
     `;
   }}
 `;
 
-export default function Icon({ size = "sm", children }: IconProps) {
-  return <IconContainer size={size}>{children}</IconContainer>;
+export default function Icon({ children }: IconProps) {
+  return <IconContainer>{children}</IconContainer>;
 }
